ec174d1f67a5c1ba2a68bc87dcf80edf3bcd16b0
Alexander Dyankov <alexander.dyankov@gmail.com>
2025-05-07 11:16:01 +0300

    Added a controller for the threads

---
+++ project/fastApiProject/controller/thread_controller.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter
+from starlette.responses import JSONResponse
+
+thread_router = APIRouter()
+
+@thread_router.get("/threads")
+async def get_threads():
+    return JSONResponse(status_code=200)
+
+@thread_router.get("/threads/{id}")
+async def get_threads():
+    return JSONResponse(status_code=200)
+
+@thread_router.post("/threads")
+async def post_thread():
+    return JSONResponse(status_code=201)
+
+@thread_router.delete("/threads/{id}")
+async def delete_thread():
+    return JSONResponse(status_code=204)
