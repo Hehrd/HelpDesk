5a7e9ee5fe32c6fc74c58300e1b507789209d7a6
Alexander Dyankov <alexander.dyankov@gmail.com>
2025-05-28 10:52:23 +0300

    Updated the exception handler with the new exceptions

--- project/error/exception_handler.py
+++ project/error/exception_handler.py
@@ -1,12 +1,18 @@
 from starlette.responses import JSONResponse
 
+from project.error.bug_not_found_exception import BugNotFoundException
 from project.error.email_already_in_use_exception import EmailAlreadyInUseException
 from project.error.email_not_in_organization_exception import EmailNotInOrganizationException
 from project.error.insufficient_data_exception import InsufficientDataException
 from project.error.invalid_jwt_exception import InvalidJWTException
+from project.error.log_not_found_exception import LogNotFoundException
 from project.error.missing_required_fields_exception import MissingRequiredFieldsException
 from project.error.null_body_exception import NullBodyException
 from fastapi import Request, FastAPI
+
+from project.error.thread_not_found_exception import ThreadNotFoundException
+from project.error.user_is_not_thread_creator_exception import UserIsNotThreadCreatorException
+from project.error.user_not_found_exception import UserNotFoundException
 
 
 def register_error_handler(app: FastAPI):
@@ -33,3 +39,23 @@ def register_error_handler(app: FastAPI):
     @app.exception_handler(NullBodyException)
     def null_body(request: Request, exc: NullBodyException):
         return JSONResponse(status_code=422, content={"message": exc.msg})
+
+    @app.exception_handler(UserNotFoundException)
+    def user_not_found(request: Request, exc:UserNotFoundException):
+        return JSONResponse(status_code=404, content={"message": exc.msg})
+
+    @app.exception_handler(UserIsNotThreadCreatorException)
+    def user_is_not_thread_creator(request: Request, exc:UserIsNotThreadCreatorException):
+        return JSONResponse(status_code=403, content={"message": exc.msg})
+
+    @app.exception_handler(ThreadNotFoundException)
+    def thread_not_found(request: Request, exc:ThreadNotFoundException):
+        return JSONResponse(status_code=404, content={"message": exc.msg})
+
+    @app.exception_handler(BugNotFoundException)
+    def bug_not_found(request: Request, exc: BugNotFoundException):
+        return JSONResponse(status_code=404, content={"message": exc.msg})
+
+    @app.exception_handler(LogNotFoundException)
+    def log_not_found(request: Request, exc: LogNotFoundException):
+        return JSONResponse(status_code=404, content={"message": exc.msg})
